953eebd935145847cedb59991b945db56f180eb6
Dalton Felipe <datonphellipe@gmail.com>
2021-06-02 09:53:13 -0300

    Atualiza versao do core do adonis

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -14,7 +14,7 @@
 */
 
 import Logger from '@ioc:Adonis/Core/Logger'
-import { HttpExceptionHandler } from './HttpExceptionHandler'
+import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   protected statusPages = {
