2d45d09531b7c96a98b89057e44c4efa90b62fac
Dalton Felipe <datonphellipe@gmail.com>
2021-08-30 11:36:59 -0300

    add relatorio de atividades por equipamento

---
+++ app/Controllers/Http/Admin/Reports/AtividadesEquipamentosController.ts
@@ -0,0 +1,61 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Equipamento from 'App/Models/Equipamento'
+// import AtividadeFuncionarioExcelService from 'App/Services/AtividadeFuncionarioExcelService'
+// import AtividadeFuncionarioService from 'App/Services/AtividadeFuncionarioService'
+import AtividadeEquipamentoService from 'App/Services/AtividadeEquipamentoService'
+
+export default class AtividadesEquipamentosController {
+  public async index({ view, request, session, response }: HttpContextContract) {
+    const contratoId: number = session.get('contratoId')
+
+    const { equipamentoId, initialDate, finalDate, format } = request.qs()
+    const equipamentos = await Equipamento.query().apply((scopes) => scopes.isSonda())
+
+    if (!initialDate || !finalDate || !equipamentoId) {
+      return view.render('admin/reports/atividades_equipamentos/index', {
+        equipamentos: equipamentos.map((i) => i.toJSON()),
+      })
+    }
+
+    const service = new AtividadeEquipamentoService(
+      contratoId,
+      equipamentoId,
+      initialDate,
+      finalDate
+    )
+    const { atividadesEquipamento, atividades, totalAtividades } = await service.build()
+
+    // download excel
+    if (format === 'excel') {
+      //   const excelService = new AtividadeFuncionarioExcelService(
+      //     equipamentoId,
+      //     initialDate,
+      //     finalDate,
+      //     {
+      //       atividadesUsuario,
+      //       atividades,
+      //       totals,
+      //     }
+      //   )
+      //   const workbook = await excelService.build()
+      //   const excelReportBuffer = await workbook.xlsx.writeBuffer()
+      //   const filename = `RelatorioAtividadesFuncionario_${DateTime.fromISO(initialDate).toFormat(
+      //     'dd-MM-yyyy'
+      //   )}_${DateTime.fromISO(finalDate).toFormat('dd-MM-yyyy')}.xlsx`
+      //   return response
+      //     .header(
+      //       'Content-Type',
+      //       'application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      //     )
+      //     .header('Content-Disposition', `attachment; filename="${filename}"`)
+      //     .send(excelReportBuffer)
+    }
+
+    return view.render('admin/reports/atividades_equipamentos/index', {
+      equipamentos: equipamentos.map((i) => i.toJSON()),
+      atividadesEquipamento,
+      totalAtividades,
+      atividades,
+    })
+  }
+}
