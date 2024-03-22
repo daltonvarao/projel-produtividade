527319423852ea85d6684736148a20326cf9d5ed
Jonathan Bispo <jfb0101@gmail.com>
2024-03-22 10:11:15 -0300

    ajuste na geracao da planilha de producao

--- analysis/production_report.py
+++ analysis/production_report.py
@@ -53,7 +53,37 @@ def execute_query(dbname, user, password, host, port, query):
 
     return results_as_dict
 
-def load_production_data(
+def carregar_atividades_funcionarios(
+      dbname, user, password, host, port, initialDate, finalDate, contractId
+):
+  sql = """
+    select u.nome as funcionario , c.titulo as cargo, r."data" , a.descricao  as atividade, f.nome as furo, ar.quantidade
+    from atividade_rdos ar
+    inner join rdo_users ru on ru.rdo_id  = ar.rdo_id
+    inner join rdos r on r.id = ar.rdo_id
+    inner join atividades a on a.id = ar.atividade_id
+    inner join furos f on f.id = ar.furo_id
+    inner join users u on u.id = ru.user_id
+    inner join cargos c on c.id  = u.cargo_id
+    where a.tipo  = 'produtiva'
+    and r."data" between '{initialDate}' and '{finalDate}'
+    and r.contrato_id = {contractId}
+    and f.invalid  is false
+    order by u.nome, r.data, a.descricao , f.nome
+    """.format(
+      initialDate=initialDate,
+      finalDate=finalDate,
+      contractId=contractId
+    )
+
+  data = execute_query(dbname, user, password, host, port, sql)
+
+  df = pd.DataFrame(data)
+
+  return df
+
+
+def carregar_dados_resumo_memoria(
       dbname,
       user,
       password,
@@ -99,7 +129,7 @@ def load_production_data(
     df['funcionario'] = df['funcionario'].apply(lambda x: x.strip())
     df['quantidade'] = df['quantidade'].astype(float)
     df['valor_unitario'] = df['valor_unitario'].astype(float)
-    
+
 
     return df
 
@@ -116,8 +146,6 @@ def gerar_memoria_de_calculo_por_funcionario(df, nome_funcionario):
 
   df_funcionario_agregado['sub_total'] = df_funcionario_agregado['quantidade'] * df_funcionario_agregado['valor_unitario']
 
-  
-
 
   def gerar_total_geral(df):
       s = pd.Series(np.nan, index=df.index)
@@ -130,10 +158,32 @@ def gerar_memoria_de_calculo_por_funcionario(df, nome_funcionario):
 
   return df_funcionario_agregado
 
+def gerar_atividades_por_funcionario(df,nome_funcionario):
+  df_funcionario = df[df['funcionario'] == nome_funcionario]
+
+  df_funcionario_agregado = df_funcionario.pivot_table(
+      index = ['funcionario', 'data', 'atividade', 'furo'],
+      values = ['quantidade']
+  )
+
+  return df_funcionario_agregado
+
+def gerar_dfs_atividades_por_funcionario(df):
+
+  dfs = []
+
+  for funcionario in df['funcionario'].unique():
+      dfs.append(
+          (funcionario, gerar_atividades_por_funcionario(df, funcionario))
+      )
+
+  return dfs
+
+
 def gerar_resumo_memoria_completo(dbname, user, password,host,port, initialDate, finalDate, contractId):
   resumo_memoria_por_funcionario = []
 
-  df = load_production_data(dbname, user, password, host, port, initialDate,finalDate,contractId)
+  df = carregar_dados_resumo_memoria(dbname, user, password, host, port, initialDate,finalDate,contractId)
 
   for funcionario in df['funcionario'].unique():
       resumo_memoria_por_funcionario.append(
@@ -144,18 +194,37 @@ def gerar_resumo_memoria_completo(dbname, user, password,host,port, initialDate,
 
   return resumo_memoria_completo
 
-def exportar_resumo_memoria_para_excel(resumo_memoria_completo, arquivo_saida):
-  resumo_memoria_completo['quantidade'] = resumo_memoria_completo['quantidade'].astype(str).str.replace('.',',')
+def exportar_para_excel(resumo_memoria_completo, atividades_por_funcionario, arquivo_saida):
 
-  resumo_memoria_completo['valor_unitario'] = resumo_memoria_completo['valor_unitario'].astype(str).str.replace('.',',')
+  def ajustar_resumo_memoria_completo():
+    resumo_memoria_completo['quantidade'] = resumo_memoria_completo['quantidade'].astype(str).str.replace('.',',')
 
-  resumo_memoria_completo['sub_total'] = resumo_memoria_completo['sub_total'].astype(str).str.replace('.',',')
+    resumo_memoria_completo['valor_unitario'] = resumo_memoria_completo['valor_unitario'].astype(str).str.replace('.',',')
 
-  resumo_memoria_completo['total_geral'] = resumo_memoria_completo['total_geral'].astype(str).str.replace('.',',')
+    resumo_memoria_completo['sub_total'] = resumo_memoria_completo['sub_total'].astype(str).str.replace('.',',')
 
-  resumo_memoria_completo['total_geral'] = resumo_memoria_completo['total_geral'].str.replace('nan','')
+    resumo_memoria_completo['total_geral'] = resumo_memoria_completo['total_geral'].astype(str).str.replace('.',',')
 
-  resumo_memoria_completo.to_excel(arquivo_saida)
+    resumo_memoria_completo['total_geral'] = resumo_memoria_completo['total_geral'].str.replace('nan','')
+
+    return resumo_memoria_completo
+
+  writer = pd.ExcelWriter(arquivo_saida, engine='xlsxwriter')
+
+  resumo_memoria_completo = ajustar_resumo_memoria_completo()
+
+  resumo_memoria_completo.to_excel(writer, sheet_name='Resumo Memória')
+
+  indice_planilha = 1
+
+  for nome_funcionario, df_atividades in atividades_por_funcionario:
+    nome_planilha = nome_funcionario.split()[0] + f"_{indice_planilha}"
+
+    df_atividades.to_excel(writer, sheet_name=nome_planilha)
+
+    indice_planilha += 1
+
+  writer.close()
 
 def executando_no_jupyter():
   return 'get_ipython' in globals()
@@ -191,7 +260,9 @@ def executar_como_script():
       contractId=args.contractId
   )
 
-  exportar_resumo_memoria_para_excel(resumo_memoria_completo, args.target_excel_file)
+
+
+  exportar_para_excel(resumo_memoria_completo, args.target_excel_file)
 #%%
 
 if executando_no_jupyter():
@@ -210,6 +281,41 @@ if executando_no_jupyter():
    )
 
    resumo_memoria_completo
+
+#%%
+
+if executando_no_jupyter():
+  load_dotenv(r'.\dados.env',override=True)
+
+  initialDate = '2023-12-21'
+  finalDate = '2024-01-20'
+  contractId = 1
+
+  resumo_memoria_completo = gerar_resumo_memoria_completo(
+    dbname=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    initialDate=initialDate,
+    finalDate=finalDate,
+    contractId=contractId
+  )
+
+  atividades_funcionarios = carregar_atividades_funcionarios(
+    dbname=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    initialDate=initialDate,
+    finalDate=finalDate,
+    contractId=contractId
+  )
+
+  dfs_atividades_por_funcionario = gerar_dfs_atividades_por_funcionario(atividades_funcionarios)
+
+  exportar_para_excel(resumo_memoria_completo, dfs_atividades_por_funcionario, 'resumo_memoria_completo.xlsx')
 
 #%%
 
