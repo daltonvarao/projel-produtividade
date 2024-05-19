2eaa42c9a7217047b023149e91a135eefb1908c0
Jonathan Bispo <jfb0101@gmail.com>
2024-05-19 15:09:03 -0300

    inclusao de script para build na producao correcoes para build de producao

--- app/Controllers/Http/Admin/Reports/AtividadeRdoUsersController.ts
+++ app/Controllers/Http/Admin/Reports/AtividadeRdoUsersController.ts
@@ -1,8 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
-import AtividadeFuncionarioExcelService from 'App/Services/AtividadeFuncionarioExcelService'
+
 import AtividadeFuncionarioService from 'App/Services/AtividadeFuncionarioService'
-import { DateTime } from 'luxon'
+
 const fs = require('fs').promises
 import Env from '@ioc:Adonis/Core/Env'
 import Application from '@ioc:Adonis/Core/Application'
@@ -93,25 +93,7 @@ export default class AtividadeRdoUsersController {
 
       return response.attachment(excelFilePath)
 
-      // const excelService = new AtividadeFuncionarioExcelService(userId, initialDate, finalDate, {
-      //   atividadesUsuario,
-      //   atividades,
-      //   totals,
-      // })
-
-      // const workbook = await excelService.build()
-      // const excelReportBuffer = await workbook.xlsx.writeBuffer()
-      // const filename = `RelatorioAtividadesFuncionario_${DateTime.fromISO(initialDate).toFormat(
-      //   'dd-MM-yyyy'
-      // )}_${DateTime.fromISO(finalDate).toFormat('dd-MM-yyyy')}.xlsx`
-
-      // return response
-      //   .header(
-      //     'Content-Type',
-      //     'application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-      //   )
-      //   .header('Content-Disposition', `attachment; filename="${filename}"`)
-      //   .send(excelReportBuffer)
+      
     }
 
     return view.render('admin/reports/atividade_rdo_users/index', {
---
+++ build-app-producao.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIRETORIO_PROJETO="/home/projel/projel-produtividade"
+
+cd $DIRETORIO_PROJETO
+
+cp -au $DIRETORIO_PROJETO/build/.env /home/projel/.env-producao-backup
+
+npm run build
+
+cp /home/projel/.env-producao-backup $DIRETORIO_PROJETO/build/.env
+
+mkdir -p $DIRETORIO_PROJETO/build/analysis
+
+cp $DIRETORIO_PROJETO/analysis/production_report.py $DIRETORIO_PROJETO/build/analysis
+
+pm2 restart server
